558cae8351a620819285cee0f22ba0d64feead76
sota-U <sota.basketball2002@icloud.com>
2021-08-03 20:43:35 +0900

    情報投稿機能(1)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,27 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
 
   def index
-    @tweets = Tweet.all
+    @tweet = Tweet.all.order(created_at: :DESC)
   end
 
   def new
-    @tweets = Tweet.new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :text, images: []).merge(user_id: current_user.id)
   end
 end
