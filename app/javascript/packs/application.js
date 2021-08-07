4bc989074b52e2a7bc5f9e181cf57db87d013cea
sota-U <sota.basketball2002@icloud.com>
2021-08-07 15:29:17 +0900

    情報投稿詳細、編集機能(1)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("../edit")
 require("../preview")
 require("../preview2")
 require("../preview3")
+require("../swiper")
+require("../swiper-bundle.min")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
